cc8079c184bafbcafa50feb4413c4ffc8942d368
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2025-02-22 16:41:40 +0900

    final

--- total.py
+++ total.py
@@ -11,7 +11,7 @@ llm = ChatOllama(model='llama3.2:1b')
 # print(llm.invoke('what is the capital of China?'))
 
 output_parser = StrOutputParser()
-print(output_parser.invoke(llm.invoke('what is the capital of China?')))
+# print(output_parser.invoke(llm.invoke('what is the capital of China?')))
 
 # chain 1 - 나라를 제시하면 그 나라에서 유명한 음식을 답하기
 ## 프롬프트
@@ -32,5 +32,5 @@ recipe_chain = prompt_template_recipe | llm | output_parser
 # print(recipe_chain.invoke({"food":"pizza"}))
 
 
-total_chain = {"country":RunnablePassthrough()} | {"food":food_chain} | recipe_chain
-print(total_chain.invoke('korea'))
+total_chain = {"food":food_chain} | recipe_chain
+print(total_chain.invoke('Italy'))
